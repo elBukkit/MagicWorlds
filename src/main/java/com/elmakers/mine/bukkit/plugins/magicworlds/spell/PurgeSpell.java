68ff3a6eb9a9594be9d4a442a6048c408f040f4c
Nathan Wolf <nathan@WolfBook.local>
2014-04-17 14:35:03 -0700

    Add Purge spell
    Mostly as an example of making custom spells for Magic

---
+++ src/main/java/com/elmakers/mine/bukkit/plugins/magicworlds/spell/PurgeSpell.java
@@ -0,0 +1,33 @@
+package com.elmakers.mine.bukkit.plugins.magicworlds.spell;
+
+import java.util.Collection;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+import com.elmakers.mine.bukkit.plugins.magic.spell.SpellResult;
+import com.elmakers.mine.bukkit.plugins.magic.spell.TargetingSpell;
+import com.elmakers.mine.bukkit.utilities.Target;
+
+public class PurgeSpell extends TargetingSpell {
+
+	@Override
+	public SpellResult onCast(ConfigurationSection parameters) {
+		Target target = getTarget();
+		if (!target.hasEntity()) {
+			return SpellResult.NO_TARGET;
+		}
+		Entity targetEntity = target.getEntity();
+		if (targetEntity instanceof Player) {
+			Player targetPlayer = (Player)targetEntity;
+			targetPlayer.kickPlayer(getMessage("cast_player_message"));
+		} else {
+			Collection<? extends Entity> entities = targetEntity.getWorld().getEntitiesByClass(targetEntity.getClass());
+			for (Entity entity : entities) {
+				entity.remove();
+			}
+		}
+		return SpellResult.CAST;
+	}
+}
