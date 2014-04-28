80f907886edcd82d1f83828b45c0b02f19a6e4f2
Nathan Wolf <nathan@elmakers.com>
2014-04-28 14:36:30 -0700

    Remove MagicPlugin as a build depedency
    Yay, MagicLib!

--- src/main/java/com/elmakers/mine/bukkit/plugins/magicworlds/spell/PurgeSpell.java
+++ src/main/java/com/elmakers/mine/bukkit/plugins/magicworlds/spell/PurgeSpell.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 import com.elmakers.mine.bukkit.api.spell.SpellResult;
-import com.elmakers.mine.bukkit.plugins.magic.spell.TargetingSpell;
+import com.elmakers.mine.bukkit.spell.TargetingSpell;
 import com.elmakers.mine.bukkit.utility.Target;
 
 public class PurgeSpell extends TargetingSpell {
