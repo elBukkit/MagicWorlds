1263ebdf57638a9ffa73e99a13dd085cb65fa6c7
Nathan Wolf <nathan@elmakers.com>
2016-01-05 08:06:54 -0800

    Add health and custom name options for replaced mobs

--- src/main/java/com/elmakers/mine/bukkit/magicworlds/spawn/builtin/ReplaceRule.java
+++ src/main/java/com/elmakers/mine/bukkit/magicworlds/spawn/builtin/ReplaceRule.java
@@ -11,13 +11,14 @@ import org.bukkit.entity.Ocelot;
 import org.bukkit.entity.Ocelot.Type;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.entity.Skeleton.SkeletonType;
-import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.Plugin;
 
 public class ReplaceRule extends EquipmentRule {
     protected EntityType 		replaceWith;
     protected String			entitySubType;
     protected boolean			docile;
+	protected double 			health;
+	protected String 			name;
 
     @Override
     public boolean load(String key, ConfigurationSection parameters, MagicWorldsController controller)
@@ -25,6 +26,8 @@ public class ReplaceRule extends EquipmentRule {
     	if (!super.load(key, parameters, controller)) return false;
     	String entityName = parameters.getString("replace_type");
     	entitySubType = parameters.getString("replace_sub_type");
+		name = parameters.getString("name");
+		health = parameters.getDouble("health", 0);
 
     	replaceWith = parseEntityType(entityName);
 		if (replaceWith == null) {
@@ -46,8 +49,6 @@ public class ReplaceRule extends EquipmentRule {
     @Override
     public LivingEntity onProcess(Plugin plugin, LivingEntity entity) {
         Entity result = entity.getWorld().spawnEntity(entity.getLocation(), replaceWith);
-        result.setMetadata("docile", new FixedMetadataValue(plugin, true));
-        
         if (entitySubType != null && entitySubType.length() > 0) {
    			try {
 	           	switch (replaceWith) {
@@ -83,6 +84,15 @@ public class ReplaceRule extends EquipmentRule {
 		}
 		LivingEntity li = (LivingEntity)result;
 		setEquipment(li);
+
+		if (health > 0) {
+			li.setMaxHealth(health);
+			li.setHealth(health);
+		}
+		if (name != null && !name.isEmpty()) {
+			li.setCustomName(name);
+			li.setCustomNameVisible(true);
+		}
         return li;
     }
 }
