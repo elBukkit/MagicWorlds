92d8b9312caf8eec6d10aea5a65fb9efdaea9665
Nathan Wolf <nathan@elmakers.com>
2016-04-15 11:37:11 -0700

    Fix load-issue order with EquipmentRule and Magic dependency

--- src/main/java/com/elmakers/mine/bukkit/magicworlds/spawn/builtin/EquipmentRule.java
+++ src/main/java/com/elmakers/mine/bukkit/magicworlds/spawn/builtin/EquipmentRule.java
@@ -1,9 +1,7 @@
 package com.elmakers.mine.bukkit.magicworlds.spawn.builtin;
 
 import com.elmakers.mine.bukkit.entity.EntityData;
-import com.elmakers.mine.bukkit.magicworlds.MagicWorldsController;
 import com.elmakers.mine.bukkit.magicworlds.spawn.SpawnRule;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.plugin.Plugin;
 
@@ -11,13 +9,11 @@ public class EquipmentRule extends SpawnRule {
     protected EntityData equipment;
 
     @Override
-    public boolean load(String key, ConfigurationSection parameters, MagicWorldsController controller)
+    public void finalizeLoad(String worldName)
     {
-    	if (!super.load(key, parameters, controller)) return false;
         equipment = new EntityData(controller.getMagic().getController(), parameters);
         controller.getLogger().info(" Replacing equipment of : " + targetEntityType.name() + " at y > " + minY
                 + " at a " + (percentChance * 100) + "% chance");
-        return true;
     }
 
     protected void setEquipment(LivingEntity entity) {
